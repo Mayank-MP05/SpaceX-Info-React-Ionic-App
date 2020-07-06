d2bc9707bf38e9af8bfc2b7b74f1e95e1ca51065
Mayank_MP5 <mayank5pande@gmail.com>
2020-07-06 21:23:42 +0530

    Launchpad and Ship Single Component Proto

--- src/components/singles/ship.tsx
+++ src/components/singles/ship.tsx
@@ -1,16 +1,46 @@
-import React from 'react';
+import React from "react";
 import { useParams } from "react-router-dom";
+import {
+  IonButton,
+  IonButtons,
+  IonCard,
+  IonHeader,
+  IonPage,
+  IonToolbar,
+  IonMenuButton,
+  IonTitle,
+  IonContent,
+} from "@ionic/react";
 interface LaunchProps {
   id: string;
 }
 
 const Shipcomponent: React.FC<LaunchProps> = () => {
-  const { id } = useParams<{ id: string; }>();
+  const { id } = useParams<{ id: string }>();
 
   return (
-    <div className="container">
-      <h1>Ship Component with ID : {id}</h1>
-    </div>
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot='start'>
+            <IonMenuButton />
+          </IonButtons>
+          <IonTitle>Ship</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
+      <IonContent>
+        <IonHeader collapse='condense'>
+          <IonToolbar>
+            <IonTitle size='large'>Ship</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <div className='container'>
+          <h1>Ship Component with ID : {id}</h1>
+        </div>
+        <IonButton expand='block'>Load More Data ...</IonButton>
+      </IonContent>
+    </IonPage>
   );
 };
 
